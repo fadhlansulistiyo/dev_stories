9ef0f5b913587769624d96964a19e9edbe04d490
Fadhlan Sulistiyo Hidayat <fadhlansulistiyo@gmail.com>
2024-11-17 20:36:16 +0700

    feat: add login & register feature

--- lib/screen/login/login_screen.dart
+++ lib/screen/login/login_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import '../../provider/auth_provider.dart';
+import 'package:provider/provider.dart';
 
 class LoginScreen extends StatefulWidget {
   final Function() onLogin;
@@ -18,9 +20,115 @@ class _LoginScreenState extends State<LoginScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final authProvider = context.watch<AuthProvider>();
+    final isLoadingLogin = authProvider.isLoadingLogin;
+
     return Scaffold(
       appBar: AppBar(),
-      body: Center(),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              /*App Logo*/
+              Center(
+                child: Image.asset(
+                  'assets/dicoding_story_logo.png',
+                  width: 200,
+                ),
+              ),
+              const SizedBox(height: 16),
+              const Row(
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Login',
+                        style:
+                            TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        'Login to continue using the App.',
+                        style: TextStyle(fontSize: 16),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+              const SizedBox(height: 16),
+              TextFormField(
+                controller: emailController,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your email.';
+                  }
+                  return null;
+                },
+                decoration: const InputDecoration(
+                  hintText: "Email",
+                ),
+              ),
+              const SizedBox(height: 8),
+              TextFormField(
+                controller: passwordController,
+                obscureText: true,
+                decoration: const InputDecoration(
+                  hintText: "Password",
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your password.';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 8),
+              isLoadingLogin
+                  ? const Center(child: CircularProgressIndicator())
+                  : ElevatedButton(
+                      onPressed: () async {
+                        if (formKey.currentState?.validate() ?? false) {
+                          final result = await authProvider.login(
+                              email: emailController.text.trim(),
+                              password: passwordController.text.trim());
+
+                          if (!context.mounted) return;
+
+                          if (result) {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(
+                                content: Text(authProvider.message ??
+                                    'Login successful.'),
+                                backgroundColor: Colors.green,
+                              ),
+                            );
+                            widget.onLogin();
+                          } else {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(
+                                content: Text(
+                                    authProvider.message ?? 'Login failed.'),
+                                backgroundColor: Colors.red,
+                              ),
+                            );
+                          }
+                        }
+                      },
+                      child: const Text("LOGIN"),
+                    ),
+              const SizedBox(height: 8),
+              OutlinedButton(
+                onPressed: () => widget.onRegister(),
+                child: const Text("REGISTER"),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 
